77c40f48e9e5c8621e2b4fa5e72b4f8082b53490
AlMaksimov <almaksimov@sportmaster.ru>
2023-12-22 21:19:42 +0300

    hw13

---
+++ hw13/src/main/java/ru/otus/hw/models/BookAuthorGenre.java
@@ -0,0 +1,30 @@
+package ru.otus.hw.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "v_books")
+public class BookAuthorGenre {
+    private Long id;
+
+    @Column(name = "book_title")
+    private String title;
+
+    @Column(name = "author_full_name")
+    private String author;
+
+    @Column(name = "genre_name")
+    private String genre;
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+}
